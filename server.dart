2e1b33842c39b482ed1e9be7cc1e50b7c15feb29
AsherHoltham <asher.holtham.github@gmail.com>
2025-04-07 06:12:50 -0700

    Starting server logic

--- server.dart
+++ server.dart
@@ -3,6 +3,27 @@ import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'dart:io';
 
 //const port = int.fromEnvironment('APP_PORT', defaultValue: 9203);
+enum GameState { setup, firstPlayerTurn, secondPlayerTurn, endOfGame }
+
+class Game {
+  GameState mState = GameState.setup;
+  Map<int, int> mBoardData = {};
+  Game() {
+    Map.fromIterable(
+      List.generate(100, (index) => index),
+      key: (item) => item,
+      value: (item) => 0,
+    );
+    mBoardData[52] = 11;
+    mBoardData[53] = 11;
+    mBoardData[56] = 11;
+    mBoardData[57] = 11;
+    mBoardData[42] = 11;
+    mBoardData[43] = 11;
+    mBoardData[46] = 11;
+    mBoardData[47] = 11;
+  }
+}
 
 void main() async {
   final portString = Platform.environment['APP_PORT'] ?? '9203';
